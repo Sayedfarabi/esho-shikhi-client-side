d8e2ef71fde287af9f1c9f474a0aa722f1dc4662
Sayedfarabi <sayedfarabi6099@gmail.com>
2022-10-25 18:47:05 +0600

    Log in and Register Page done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div>
-            <h1>This is Log in Page</h1>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content">
+
+                    <div className="card w-full shadow-2xl bg-base-100">
+                        <h2 className='text-center text-4xl py-3 text-sky-600'>Log in Now !!</h2>
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <Link to={''} className="label-text-alt link link-hover">Forgot password?</Link>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
         <div>
-            <h1>This is Register Page</h1>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content">
+
+                    <div className="card w-full shadow-3xl bg-base-100">
+                        <h2 className='text-center text-4xl py-3 text-sky-600'>Register Now !!</h2>
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="Type your name" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo</span>
+                                </label>
+                                <input type="text" name='photo' placeholder="Select Your Photo" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <p>Have an account ? Please <Link to={''} className="label-text-alt link link-hover text-indigo-600 text-lg">Log in</Link> </p>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Register</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
