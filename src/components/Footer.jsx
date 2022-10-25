8016cf4d2b46816b81a17ea16b31d24e520c8ff4
Sayedfarabi <sayedfarabi6099@gmail.com>
2022-10-25 11:08:33 +0600

    tailwind css setup done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <p><small>All reserved copyright 2022</small></p>
+        <div className='py-8'>
+            <p className='text-center'><small>All reserved copyright 2022</small></p>
         </div>
     );
 };
